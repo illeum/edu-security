e6e8b096135b8cd5b7c162a987bd538daec492d8
illeum <kimsunga0322@gmail.com>
2025-06-02 23:01:37 +0900

    LoginService: 로그인 시 JMT 발급 추가

--- server/src/main/java/com/security/edu/application/login/dto/LoginResponse.java
+++ server/src/main/java/com/security/edu/application/login/dto/LoginResponse.java
@@ -2,12 +2,18 @@ package com.security.edu.application.login.dto;
 
 public class LoginResponse {
     private String message;
+    private String token;
 
-    public LoginResponse(String message) {
+    public LoginResponse(String message, String token) {
         this.message = message;
+        this.token = token;
     }
 
     public String getMessage() {
         return message;
+    }
+
+    public String getToken() {
+        return token;
     }
 }
--- server/src/main/java/com/security/edu/application/login/service/LoginService.java
+++ server/src/main/java/com/security/edu/application/login/service/LoginService.java
@@ -4,6 +4,7 @@ import com.security.edu.application.login.dto.LoginRequest;
 import com.security.edu.application.login.dto.LoginResponse;
 import com.security.edu.application.login.repository.UserRepository;
 import com.security.edu.domain.user.User;
+import com.security.edu.security.JwtUtil;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,21 +12,24 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class LoginService {
-
     private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
 
-    public LoginService(UserRepository userRepository) {
+    public LoginService(UserRepository userRepository, JwtUtil jwtUtil) {
         this.userRepository = userRepository;
+        this.jwtUtil = jwtUtil;
     }
 
     public ResponseEntity<LoginResponse> login(LoginRequest request) {
         User user = userRepository.findByUsername(request.getUsername());
 
         if (user != null && user.getPassword().equals(request.getPassword())) {
-            return ResponseEntity.ok(new LoginResponse("로그인 성공"));
-        } else {
+            String token = jwtUtil.generateToken(user.getUsername());
+            return ResponseEntity.ok(new LoginResponse("로그인 성공", token));
+        }
+        else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body(new LoginResponse("아이디 또는 비밀번호가 틀렸습니다"));
+                    .body(new LoginResponse("아이디 또는 비밀번호가 틀렸습니다", null));
         }
     }
 }
